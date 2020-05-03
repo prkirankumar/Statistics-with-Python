08b7b7586e5c351ef1b3e617dbc166b60b1583b3
prkirankumar <reddykkp@hotmail.com>
2020-05-04 00:15:58 +0530

    demo Binomial Distribution

---
+++ BinomialDistribution.py
@@ -0,0 +1,12 @@
+# from numpy import random
+
+# x = random.binomial(n=10, p=0.5, size=10)
+# print(x)
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.binomial(n=10, p=0.5, size=1000), hist=True, kde=False)
+
+plt.show()
